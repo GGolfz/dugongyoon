23e17ddebf42d09258e42c4757e4fcdfb1b2f45a
GGolfz <wisarutgolf11@gmail.com>
2019-12-16 21:52:50 +0700

    Updates

---
+++ precache-manifest.9de7789c84a217a9307974053d68118b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ebddd3b6fd9370e8180236c28a1f5c",
+    "url": "/dugongyoon/index.html"
+  },
+  {
+    "revision": "0faff7262a9117f78a3d",
+    "url": "/dugongyoon/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "974985de6a09adf0a71b",
+    "url": "/dugongyoon/static/css/main.6ec2c575.chunk.css"
+  },
+  {
+    "revision": "0faff7262a9117f78a3d",
+    "url": "/dugongyoon/static/js/2.0db1357b.chunk.js"
+  },
+  {
+    "revision": "974985de6a09adf0a71b",
+    "url": "/dugongyoon/static/js/main.c43788f8.chunk.js"
+  },
+  {
+    "revision": "eaee55accdff70e1c74e",
+    "url": "/dugongyoon/static/js/runtime-main.a6fd0d75.js"
+  }
+]);
